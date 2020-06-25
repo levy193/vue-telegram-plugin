ef57b0781ac60b8792ee3dc63605bb82668435e2
levy193 <quangdong.luu@gmail.com>
2020-06-26 00:41:29 +0700

    update

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import VueTelegram from '../src/main'
+import VueTelegram from '../src/index'
 
 Vue.config.productionTip = false
 
